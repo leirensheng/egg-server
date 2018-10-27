de79bd77c44e14db4e77f42ee05ee588b74efe1a
leirensheng <leirensheng@163.com>
2018-10-27 09:33:28 +0800

    feat:base

---
+++ app/middleware/onerror.js
@@ -0,0 +1,13 @@
+
+module.exports=()=>{
+    return async function onerror(ctx, next) {
+        try {
+          await next();
+        } catch (err) {
+            console.log('------错-------',err)
+          ctx.app.emit('error', err);
+          ctx.body = 'server error';
+          ctx.status = err.status || 500;
+        }
+      }
+} 
