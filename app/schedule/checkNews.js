bd046a119f22aa1af5979dfdf9be711265b6cdf0
leirensheng <leirensheng@163.com>
2018-11-08 00:19:07 +0800

    feat: 1h

--- app/schedule/checkNews.js
+++ app/schedule/checkNews.js
@@ -2,7 +2,7 @@
 // task 的入参为 ctx，匿名的 Context 实例，可以通过它调用 service 等。
 module.exports = {
   schedule: {
-    interval: '10s', // 1 分钟间隔
+    interval: '1h', // 1 分钟间隔
     type: 'worker', // 指定所有的 worker 都需要执行
     // 每三小时准点执行一次
     //  cron: '0 0 */3 * * *',
