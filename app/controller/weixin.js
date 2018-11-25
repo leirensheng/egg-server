a8efb26d248bb1db4069228b5f07a8c57df43478
leirensheng <leirensheng@163.com>
2018-11-25 23:19:36 +0800

    feat: 微信的接口都尝试验证

--- app/controller/weixin.js
+++ app/controller/weixin.js
@@ -8,8 +8,11 @@ class WeixinController extends Controller {
     this.ctx.body = result;
   }
   dataFromWx() {
-    console.log('data From Wx', this.ctx.request.body);
-    this.ctx.body = '';
+    const result = this.service.weixin.check(this.ctx.request.query.signature, this.ctx.request.query.timestamp, this.ctx.request.query.nonce, this.ctx.request.query.echostr);
+    if (result) {
+      console.log('data From Wx', this.ctx.request.body);
+      this.ctx.body = '';
+    }
   }
   async test() {
     this.ctx.body = await this.service.weixin.getTocken();
